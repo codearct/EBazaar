95eba2bdec0e944074ad92d0bfd5fa8e1a7502ed
codearct <codearct@gmail.com>
2024-08-04 17:19:26 +0300

    Implemented endpoints and mediaTr handlers for Basket Api features.

--- src/Services/Basket/Basket.API/Basket/DeleteBasket/DeleteBasketHandler.cs
+++ src/Services/Basket/Basket.API/Basket/DeleteBasket/DeleteBasketHandler.cs
@@ -1,5 +1,24 @@
 ﻿namespace Basket.API.Basket.DeleteBasket;
 
-public class DeleteBasketHandler
+public record DeleteBasketCommand(string UserName) 
+    : ICommand<DeleteBasketResult>;
+public record DeleteBasketResult(bool IsSuccess);
+
+public class DeleteBasketCommandValidator : AbstractValidator<DeleteBasketCommand>
 {
+    public DeleteBasketCommandValidator()
+    {
+        RuleFor(x => x.UserName).NotEmpty().WithMessage("UserName is required");
+    }
+}
+
+public class DeleteBasketHandler : ICommandHandler<DeleteBasketCommand, DeleteBasketResult>
+{
+    public async Task<DeleteBasketResult> Handle(DeleteBasketCommand command, CancellationToken cancellationToken)
+    {
+        //TODO:delete basket from db and cache
+        //session.Delete<ShoppingCart>(command.UserName)
+
+        return new DeleteBasketResult(true);
+    }
 }
--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -2,6 +2,15 @@ var builder = WebApplication.CreateBuilder(args);
 
 #region Services
 
+builder.Services.AddCarter();
+
+var assembly = typeof(Program).Assembly;
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssembly(assembly);
+    config.AddOpenBehavior(typeof(ValidationBehavior<,>));
+    config.AddOpenBehavior(typeof(LoggingBehavior<,>));
+});
 
 #endregion
 
@@ -9,6 +18,7 @@ var app = builder.Build();
 
 #region Pipelines
 
+app.MapCarter();
 
 #endregion
 

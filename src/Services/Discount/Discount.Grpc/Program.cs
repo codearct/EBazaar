d65e0bfb8c47c6da15483efe196deddab99380ae
codearct <codearct@gmail.com>
2024-08-17 18:36:49 +0300

    Added Sqlite.

--- src/Services/Discount/Discount.Grpc/Program.cs
+++ src/Services/Discount/Discount.Grpc/Program.cs
@@ -1,14 +1,27 @@
+using Discount.Grpc.Data;
 using Discount.Grpc.Services;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
+var dbConnection = builder.Configuration.GetConnectionString("Database");
 
-// Add services to the container.
+#region Services
 builder.Services.AddGrpc();
+
+builder.Services.AddDbContext<DiscountContext>(options =>
+{
+    options.UseSqlite(dbConnection);
+});
+#endregion
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+#region Pipeline
+app.UseMigration();
+
 app.MapGrpcService<DiscountService>();
+
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+#endregion
 
 app.Run();

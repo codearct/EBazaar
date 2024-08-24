a15e411d5be3f2446e6e64e3d7ba8f968725d0be
codearct <codearct@gmail.com>
2024-08-24 21:23:37 +0300

    Created IoC infrastructure.

--- src/Services/Ordering/Ordering.API/Program.cs
+++ src/Services/Ordering/Ordering.API/Program.cs
@@ -1,6 +1,15 @@
+using Ordering.API;
+using Ordering.Application;
+using Ordering.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
 #region Services
+
+builder.Services
+    .AddApplicationServices()
+    .AddInfrastructureServices(builder.Configuration)
+    .AddApiServices();
 
 #endregion
 
@@ -9,6 +18,8 @@ var app = builder.Build();
 
 
 #region Pipeline
+
+app.UseApiServices();
 
 #endregion
 

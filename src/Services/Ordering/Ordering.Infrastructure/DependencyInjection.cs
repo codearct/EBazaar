668ec5911bdf1698d2f8a7e828f690e20649c6ea
codearct <codearct@gmail.com>
2024-09-14 23:14:49 +0300

    Added dispatch domain event interceptor.

--- src/Services/Ordering/Ordering.Infrastructure/DependencyInjection.cs
+++ src/Services/Ordering/Ordering.Infrastructure/DependencyInjection.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using Microsoft.EntityFrameworkCore.Diagnostics;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Ordering.Infrastructure;
@@ -6,13 +7,17 @@ namespace Ordering.Infrastructure;
 public static class DependencyInjection
 {
     public static IServiceCollection AddInfrastructureServices(
-        this IServiceCollection services, 
+        this IServiceCollection services,
         IConfiguration configuration)
     {
         var connectionString = configuration.GetConnectionString("Database");
-        services.AddDbContext<ApplicationDbContext>(options =>
+
+        services.AddScoped<ISaveChangesInterceptor, AuditableEntityInterceptor>();
+        services.AddScoped<ISaveChangesInterceptor, DispatchDomainEventsInterceptor>();
+
+        services.AddDbContext<ApplicationDbContext>((serviceProvider, options) =>
         {
-            options.AddInterceptors(new AuditableEntityInterceptor());
+            options.AddInterceptors(serviceProvider.GetService<ISaveChangesInterceptor>()!);
             options.UseSqlServer(connectionString);
         });
 

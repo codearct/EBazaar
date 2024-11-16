ddb990f28e9d790389a9b73139cb4130efb79ad7
codearct <codearct@gmail.com>
2024-11-16 18:04:55 +0300

    Added rate limiting for ordering.

--- src/ApiGateways/YarpApiGateway/Program.cs
+++ src/ApiGateways/YarpApiGateway/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.AspNetCore.RateLimiting;
+
 var builder = WebApplication.CreateBuilder(args);
 
 #region Services
@@ -5,11 +7,21 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddReverseProxy()
     .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
 
+builder.Services.AddRateLimiter(rateLimiterOptions =>
+{
+    rateLimiterOptions.AddFixedWindowLimiter("fixed", options =>
+    {
+        options.Window = TimeSpan.FromSeconds(10);
+        options.PermitLimit = 5;
+    });
+});
 #endregion
 
 var app = builder.Build();
 
 #region Pipeline
+
+app.UseRateLimiter();
 
 app.MapReverseProxy();
 

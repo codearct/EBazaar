fae65fdde37add9fc861f776998e28658090d011
codearct <codearct@gmail.com>
2024-11-16 16:56:23 +0300

    Added Yarp.ReverseProxy

--- src/ApiGateways/YarpApiGateway/Program.cs
+++ src/ApiGateways/YarpApiGateway/Program.cs
@@ -2,11 +2,16 @@ var builder = WebApplication.CreateBuilder(args);
 
 #region Services
 
+builder.Services.AddReverseProxy()
+    .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+
 #endregion
 
 var app = builder.Build();
 
 #region Pipeline
+
+app.MapReverseProxy();
 
 #endregion
 

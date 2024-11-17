764ef82a93b5898498ed41b6ad5a4b0cd231b426
codearct <codearct@gmail.com>
2024-11-17 18:25:32 +0300

    Created view models.

---
+++ src/WebApps/Ebazaar.Web/Models/Ordering/OrderModel.cs
@@ -0,0 +1,45 @@
+﻿namespace Ebazaar.Web.Models.Ordering;
+
+public record OrderModel(
+    Guid Id,
+    Guid CustomerId,
+    string OrderName,
+    AddressModel ShippingAddress,
+    AddressModel BillingAddress,
+    PaymentModel Payment,
+    OrderStatus Status,
+    List<OrderItemModel> OrderItems);
+
+public record AddressModel(
+    string FirstName,
+    string LastName,
+    string EmailAddress,
+    string AddressLine,
+    string Country,
+    string State,
+    string ZipCode);
+
+public record OrderItemModel(
+    Guid OrderId,
+    Guid ProductId,
+    int Quantity,
+    decimal Price);
+
+public record PaymentModel(
+    string CardName,
+    string CardNumber,
+    string Expiration,
+    string Cvv,
+    int PaymentMethod);
+
+public enum OrderStatus
+{
+    Draft = 1,
+    Pending,
+    Completed,
+    Cancelled
+}
+
+public record GetOrdersResponse(PaginatedResult<OrderModel> Orders);
+public record GetOrdersByNameResponse(IEnumerable<OrderModel> Orders);
+public record GetOrdersByCustomerResponse(IEnumerable<OrderModel> Orders);

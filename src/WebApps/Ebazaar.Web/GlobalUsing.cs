d314dc05f5e90002ef08344e362e703f8eb87be3
codearct <codearct@gmail.com>
2024-12-01 17:54:22 +0300

    Added ordering requests by refit.

--- src/WebApps/Ebazaar.Web/GlobalUsing.cs
+++ src/WebApps/Ebazaar.Web/GlobalUsing.cs
@@ -1,4 +1,5 @@
 ﻿global using Ebazaar.Web.Models.Basket;
 global using Ebazaar.Web.Models.Catalog;
+global using Ebazaar.Web.Models.Ordering;
 global using Ebazaar.Web.Services;
 global using Refit;

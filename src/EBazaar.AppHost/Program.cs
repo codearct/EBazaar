5fa334d9992b9352df2c0ad1efe01ab474a278cd
codearct <codearct@gmail.com>
2025-01-19 17:15:52 +0300

    Added .net aspire.

--- src/EBazaar.AppHost/Program.cs
+++ src/EBazaar.AppHost/Program.cs
@@ -1,111 +1,48 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-var catalogDb = builder.AddPostgres("catalog-db")
-    .WithEnvironment("POSTGRES_DB", "CatalogDb")
-    .WithEnvironment("POSTGRES_USER", "postgres")
-    .WithEnvironment("POSTGRES_PASSWORD", "postgres")
-    .WithHttpEndpoint(5432)
-    .WithDataVolume("postgres_catalog")
-    .WithPgAdmin();
+var catalogDb = builder.AddPostgres("catalog-db");
 
-var basketDb = builder.AddPostgres("basket-db")
-    .WithEnvironment("POSTGRES_DB", "BasketDb")
-    .WithEnvironment("POSTGRES_USER", "postgres")
-    .WithEnvironment("POSTGRES_PASSWORD", "postgres")
-    .WithHttpEndpoint(5433)
-    .WithDataVolume("postgres_basket")
-    .WithPgAdmin();
+var basketDb = builder.AddPostgres("basket-db");
 
-var distributedCache = builder.AddRedis("distributed-cache")
-    .WithHttpEndpoint(6379);
+var distributedCache = builder.AddRedis("distributed-cache");
 
-var orderDb = builder.AddSqlServer("order-db")
-    .WithEnvironment("SA_PASSWORD", "SwN12345678")
-    .WithEnvironment("ACCEPT_EULA", "Y")
-    .WithHttpEndpoint(1433);
+var orderDb = builder.AddSqlServer("order-db");
 
-var messageBroker = builder.AddRabbitMQ("message-broker")
-    .WithEnvironment("RABBITMQ_DEFAULT_USER", "guest")
-    .WithEnvironment("RABBITMQ_DEFAULT_PASS", "guest")
-    .WithEnvironment("HOSTNAME", "ebazaar-mq")
-    .WithHttpEndpoint(5672)
-    .WithHttpsEndpoint(15672);
+var password = builder.AddParameter("password", "guest");
+var messageBroker = builder.AddRabbitMQ("message-broker", password: password)
+    .WithHttpEndpoint(5672, 5672)
+    .WithEnvironment("MessageBroker__Host", "amqp://localhost:5672")
+    .WithEnvironment("MessageBroker__UserName", "guest")
+    .WithEnvironment("MessageBroker__Password", "guest")
+    .WithManagementPlugin();
 
 var catalogApi = builder.AddProject<Projects.Catalog_API>("catalog-api")
-    .WithReference(catalogDb)
-    .WithEnvironment("ASPNETCORE_ENVIRONMENT", "Development")
-    .WithEnvironment("ASPNETCORE_HTTP_PORTS", "8080")
-    .WithEnvironment("ASPNETCORE_HTTPS_PORTS", "8081")
-    .WithEnvironment("ConnectionStrings__Database", "Server=catalog-db;Port=5432;Database=CatalogDb;User Id=postgres;Password=postgres;Include Error Detail=true")
-    .WithHttpEndpoint(6000)
-    .WithHttpsEndpoint(6060)
+    .WithReference(catalogDb, "Database")
     .WaitFor(catalogDb);
 
-var discountGrpc = builder.AddProject<Projects.Discount_Grpc>("discount-grpc")
-    .WithEnvironment("ASPNETCORE_ENVIRONMENT", "Development")
-    .WithEnvironment("ASPNETCORE_HTTP_PORTS", "8080")
-    .WithEnvironment("ASPNETCORE_HTTPS_PORTS", "8081")
-    .WithEnvironment("ConnectionStrings__Database", "Data Source=discountdb")
-    .WithHttpEndpoint(6002)
-    .WithHttpsEndpoint(6062);
+var discountGrpc = builder.AddProject<Projects.Discount_Grpc>("discount-grpc");
 
 var basketApi = builder.AddProject<Projects.Basket_API>("basket-api")
-    .WithReference(basketDb)
-    .WithReference(distributedCache)
+    .WithReference(basketDb, "Database")
+    .WithReference(distributedCache, "Redis")
     .WithReference(messageBroker)
     .WithReference(discountGrpc)
-    .WithEnvironment("ASPNETCORE_ENVIRONMENT", "Development")
-    .WithEnvironment("ASPNETCORE_HTTP_PORTS", "8080")
-    .WithEnvironment("ASPNETCORE_HTTPS_PORTS", "8081")
-    .WithEnvironment("ConnectionStrings__Database", "Server=basket-db;Port=5432;Database=BasketDb;User Id=postgres;Password=postgres;Include Error Detail=true")
-    .WithEnvironment("ConnectionStrings__Redis", "distributed-cache:6379")
-    .WithEnvironment("GrpcSettings__DiscountUrl", "http://discount-grpc:8081")
-    .WithEnvironment("MessageBroker__Host", "amqp://ebazaar-mq:567")
-    .WithEnvironment("MessageBroker__UserNam", "guest")
-    .WithEnvironment("MessageBroker__Password", "guest")
-    .WithHttpEndpoint(6001)
-    .WithHttpsEndpoint(6061)
     .WaitFor(basketDb)
-    .WaitFor(distributedCache)
-    .WaitFor(discountGrpc)
-    .WaitFor(messageBroker);
+    .WaitFor(messageBroker)
+    .WaitFor(discountGrpc);
 
 var orderingApi = builder.AddProject<Projects.Ordering_API>("ordering-api")
-    .WithReference(orderDb)
+    .WithReference(orderDb, "Database")
     .WithReference(messageBroker)
-    .WithEnvironment("ASPNETCORE_ENVIRONMENT", "Development")
-    .WithEnvironment("ASPNETCORE_HTTP_PORTS", "8080")
-    .WithEnvironment("ASPNETCORE_HTTPS_PORTS", "8081")
-    .WithEnvironment("ConnectionStrings__Database", "Server=order-db;Database=OrderDb;User ID=sa;Password=SwN12345678;Encrypt=False;TrustServerCertificate=True")
-    .WithEnvironment("MessageBroker__Host", "amqp://ebazaar-mq:567")
-    .WithEnvironment("MessageBroker__UserNam", "guest")
-    .WithEnvironment("MessageBroker__Password", "guest")
-    .WithHttpEndpoint(6003)
-    .WithHttpsEndpoint(6063)
     .WaitFor(orderDb)
     .WaitFor(messageBroker);
 
 var yarpApiGateway = builder.AddProject<Projects.YarpApiGateway>("yarpapigateway")
     .WithReference(catalogApi)
     .WithReference(basketApi)
-    .WithReference(orderingApi)
-    .WithEnvironment("ASPNETCORE_ENVIRONMENT", "Development")
-    .WithEnvironment("ASPNETCORE_HTTP_PORTS", "8080")
-    .WithEnvironment("ASPNETCORE_HTTPS_PORTS", "8081")
-    .WithHttpEndpoint(6004)
-    .WithHttpsEndpoint(6064)
-    .WaitFor(catalogApi)
-    .WaitFor(basketApi)
-    .WaitFor(orderingApi);
+    .WithReference(orderingApi);
 
 builder.AddProject<Projects.Ebazaar_Web>("ebazaar-web")
-    .WithReference(yarpApiGateway)
-    .WithEnvironment("ASPNETCORE_ENVIRONMENT", "Development")
-    .WithEnvironment("ASPNETCORE_HTTP_PORTS", "8080")
-    .WithEnvironment("ASPNETCORE_HTTPS_PORTS", "8081")
-    .WithEnvironment("ApiSettings__GatewayAddress", "http://yarpapigateway:8080")
-    .WithHttpEndpoint(6005)
-    .WithHttpsEndpoint(6065)
-    .WaitFor(yarpApiGateway);
+    .WithReference(yarpApiGateway);
 
 builder.Build().Run();
